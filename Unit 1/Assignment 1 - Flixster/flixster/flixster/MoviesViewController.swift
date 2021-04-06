4e539fca289613854c85f67b14c92ea03b8f16ea
Radicitus <novivolatus@gmail.com>
2021-04-05 19:48:52 -0700

    Added CocoaPods to install AlamofireImage pod and allowed displaying of posters

--- Unit 1/Assignment 1 - Flixster/flixster/flixster/MoviesViewController.swift
+++ Unit 1/Assignment 1 - Flixster/flixster/flixster/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -48,6 +49,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterURL!)
         
         return cell
     }
