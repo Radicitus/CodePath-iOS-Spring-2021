c43bb7d20d206e2eeaba66f961f4e912fa4dabba
Radicitus <novivolatus@gmail.com>
2021-04-12 20:47:32 -0700

    Completed base requirements for Assignment 2

---
+++ Unit 1/Assignment 2 - Flixster/flixster/flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Cameron Sherry on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
